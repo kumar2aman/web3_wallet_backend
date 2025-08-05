8eea78318f1be82ea141bd11f5a61f8a7ccfc5bc
AMAN SINGH <2amansingh07@gmail.com>
2025-08-05 22:25:46 +0530

    removed dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createHDwallet_1 = require("./createHDwallet");
-console.log((0, createHDwallet_1.main)());
-//# sourceMappingURL=index.js.map
